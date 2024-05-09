ae0ee2ba8de5b2a456634cebb93f373bef12343a
Daniel Valls <daniel.valls.mestre@gmail.com>
2024-05-09 23:04:39 +0200

    Removed unnecesary styling

--- src/Tilt3D/utils/generateStyleText.ts
+++ src/Tilt3D/utils/generateStyleText.ts
@@ -7,18 +7,21 @@ const generateStyleText = (
   zoomScale: number = 1
 ) => {
   const brightness = tilt.y / maxTilt + 1;
-  const translateValue = Math.max(Math.abs(tilt.x), Math.abs(tilt.y)) / 2;
   const scale = (tilt.y || tilt.x) && zoomOnTilt ? zoomScale : 1;
 
   const rotateX = `rotateX(${tilt.y}deg)`;
   const rotateY = `rotateY(${tilt.x}deg)`;
-  const translateZ = `translateZ(${translateValue}px)`;
 
-  const filter = `brightness(${brightness})`;
-  const transform = `perspective(10cm) ${rotateX} ${rotateY} ${translateZ} scale(${scale})`;
-  const transition = `all 0.25s cubic-bezier(.32,.66,.72,1.58)`;
+  const style = {
+    filter: `brightness(${brightness})`,
+    transform: `perspective(10cm) ${rotateX} ${rotateY} scale(${scale})`,
+    transition: 'all 0.25s cubic-bezier(.32,.66,.72,1.58)',
+  };
 
-  return `filter: ${filter}; transform: ${transform}; transition: ${transition};`;
+  return Object.entries(style).reduce(
+    (acc, [key, value]) => `${acc} ${key}: ${value};`,
+    ''
+  );
 };
 
 export default generateStyleText;
